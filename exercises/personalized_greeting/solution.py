6f8dda29e743bf1c6306fd7481c3d7bb7b009639
Marcos0808 <marcos.dmiranda@estudiantes.matcom.uh.cu>
2026-03-10 02:41:12 -0400

    Update solution.py

--- exercises/personalized_greeting/solution.py
+++ exercises/personalized_greeting/solution.py
@@ -1,4 +1,4 @@
-SUBMIT = False
+SUBMIT = True
 
 
 def personalized_greeting(_name: str, _age: int) -> str:
@@ -11,7 +11,8 @@ def personalized_greeting(_name: str, _age: int) -> str:
     >>> personalized_greeting("Bob", 30)
     'Hello Bob, you are 30 years old'
     """
-    return ""
+    result = f"Hello {_name}, you are {_age} years old"
+    return result
 
 
 def test() -> None:
